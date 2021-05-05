e5f46861a512c5bfccdcb60bc59903602ca2574b
Andre Macola <andremacola@gmail.com>
2021-05-05 18:16:36 -0300

    fastapi init

--- server.py
+++ server.py
@@ -1,24 +1,17 @@
 import spacy
 import pytextrank
-from flask import Flask, jsonify, abort, request, send_from_directory, render_template
+from fastapi import Form, FastAPI
 from controllers.rank import Rank
 
 # instanciar o Flask
-app = Flask(__name__)
+app = FastAPI()
 
 # carregar spaCy, idiomas e configurações na memória
 nlp = spacy.load("pt_core_news_md")
 nlp.add_pipe("textrank")
 
 # Rota de ranking dos termos
-@app.route("/rank", methods=['GET', 'POST'])
-def rank():
-    params = request.form # postdata
-    data = dict()
-
-    if not 'text' in params:
-        data['error'] = '[text] parameter not found'
-        return jsonify(data)
-
-    rank = Rank(nlp, params['text'])
+@app.post("/rank")
+def rank(text: str = Form(...)):
+    rank = Rank(nlp, text)
     return rank.run()
